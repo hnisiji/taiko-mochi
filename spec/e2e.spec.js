5ea54be238f4ba3a7af25dae7f0abb1d84dd945c
hnisiji <hnisiji@gmail.com>
2018-09-25 21:02:18 +0900

    aaaaaa

--- spec/e2e.spec.js
+++ spec/e2e.spec.js
@@ -32,8 +32,10 @@ describe('www.yahoo.co.jp', () => {
 
         try {
             const data = childProcess.execSync(`${revisionInfo.executablePath} --remote-debugging-port=0 --headless 2>&1`, { timeout: 3 * 1000 });
+            console.log('success');
             console.log(data.toString());
         } catch (e) {
+            console.log('failed');
             console.log(e.stdout.toString());
         }
         done();
