d356db0301b64ebcc774817e105e1a5b80e8dabb
hnisiji <hnisiji@gmail.com>
2018-09-25 21:18:12 +0900

    aaaaaaa

--- spec/e2e.spec.js
+++ spec/e2e.spec.js
@@ -11,6 +11,8 @@ const {
     click,
     screenshot,
 } = require('taiko');
+const BrowserFetcher = require('taiko/lib/browserFetcher');
+const childProcess = require('child_process');
 
 describe('www.yahoo.co.jp', () => {
     const screenshotDir = path.join(process.cwd(), 'reports/attachments/');
@@ -18,17 +20,38 @@ describe('www.yahoo.co.jp', () => {
         mkdirp(screenshotDir, done);
     });
 
-    it('検索できる', async (done) => {
-        await openBrowser({ headless: true });
-        await goto('https://www.yahoo.co.jp');
-        await write('うおぽぉ', inputField({ id: 'srchtxt' }));
-        await click($('#srchbtn'));
-        await screenshot({ path: path.join(screenshotDir, 'uopolo.png') });
+    // beforeEach(async (done) => {
+    //     await openBrowser({ headless: true, args: '--no-sandbox' });
+    //     done();
+    // });
+
+    it(' troubleshooting', async (done) => {
+        const browserFetcher = new BrowserFetcher();
+        const revisions = await browserFetcher.localRevisions();
+        const revisionInfo = browserFetcher.revisionInfo(revisions[0]);
+
+        try {
+            const data = childProcess.execSync(`${revisionInfo.executablePath} --remote-debugging-port=0 --headless 2>&1`, { timeout: 3 * 1000 });
+            console.log('success');
+            console.log(data.toString());
+        } catch (e) {
+            console.log('failed');
+            console.log(e.stdout.toString());
+            console.log(e.stderr.toString());
+        }
         done();
     });
 
-    afterEach(async (done) => {
-        await closeBrowser();
-        done();
-    });
+    // it('検索できる', async (done) => {
+    //     await goto('https://www.yahoo.co.jp');
+    //     await write('うおぽぉ', inputField({ id: 'srchtxt' }));
+    //     await click($('#srchbtn'));
+    //     await screenshot({ path: path.join(screenshotDir, 'uopolo.png') });
+    //     done();
+    // });
+
+    // afterEach(async (done) => {
+    //     await closeBrowser();
+    //     done();
+    // });
 });
