bbd1ec445531ced1f29eb41ca30d10b1156e481d
hnisiji <hnisiji@gmail.com>
2018-09-25 21:03:52 +0900

    Output return value of openBrowser() to console

--- spec/e2e.spec.js
+++ spec/e2e.spec.js
@@ -11,8 +11,6 @@ const {
     click,
     screenshot,
 } = require('taiko');
-const BrowserFetcher = require('taiko/lib/browserFetcher');
-const childProcess = require('child_process');
 
 describe('www.yahoo.co.jp', () => {
     const screenshotDir = path.join(process.cwd(), 'reports/attachments/');
@@ -20,37 +18,22 @@ describe('www.yahoo.co.jp', () => {
         mkdirp(screenshotDir, done);
     });
 
-    // beforeEach(async (done) => {
-    //     await openBrowser({ headless: true, args: '--no-sandbox' });
-    //     done();
-    // });
-
-    it(' troubleshooting', async (done) => {
-        const browserFetcher = new BrowserFetcher();
-        const revisions = await browserFetcher.localRevisions();
-        const revisionInfo = browserFetcher.revisionInfo(revisions[0]);
-
-        try {
-            const data = childProcess.execSync(`${revisionInfo.executablePath} --remote-debugging-port=0 --headless 2>&1`, { timeout: 3 * 1000 });
-            console.log('success');
-            console.log(data.toString());
-        } catch (e) {
-            console.log('failed');
-            console.log(e.stdout.toString());
-        }
+    beforeEach(async (done) => {
+        const result = await openBrowser({ headless: true });
+        console.log(result);
         done();
     });
 
-    // it('検索できる', async (done) => {
-    //     await goto('https://www.yahoo.co.jp');
-    //     await write('うおぽぉ', inputField({ id: 'srchtxt' }));
-    //     await click($('#srchbtn'));
-    //     await screenshot({ path: path.join(screenshotDir, 'uopolo.png') });
-    //     done();
-    // });
+    it('検索できる', async (done) => {
+        await goto('https://www.yahoo.co.jp');
+        await write('うおぽぉ', inputField({ id: 'srchtxt' }));
+        await click($('#srchbtn'));
+        await screenshot({ path: path.join(screenshotDir, 'uopolo.png') });
+        done();
+    });
 
-    // afterEach(async (done) => {
-    //     await closeBrowser();
-    //     done();
-    // });
+    afterEach(async (done) => {
+        await closeBrowser();
+        done();
+    });
 });
